7a959f7a7446e1628169be75a58ac1275c996f0b
Alapan Mukherjee <alapan.mukherjee35@gmail.com>
2024-12-01 03:17:46 +0000

    Add about pages

---
+++ src/pages/about/ContactUs.jsx
@@ -0,0 +1,7 @@
+const ContactUs = () => {
+  return (
+    <h2>Our contact details</h2>
+  )
+};
+
+export default ContactUs;
---
+++ src/pages/about/DeliveryInformation.jsx
@@ -0,0 +1,7 @@
+const DeliveryInformation = () => {
+  return (
+    <h3>We deliver to your homes</h3>
+  ); 
+};
+
+export default DeliveryInformation;
