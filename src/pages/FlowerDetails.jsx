dced49c736a6fa12ca3963783655153b8b43cae3
Alapan Mukherjee <alapan.mukherjee35@gmail.com>
2024-12-01 03:14:26 +0000

    Add FlowerDetails component

---
+++ src/pages/FlowerDetails.jsx
@@ -0,0 +1,59 @@
+import { useContext, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { CartContext } from '../layouts/RootLayout';
+import './styles/FlowerDetails.css';
+
+const FlowerDetails = () => {
+  const [ selectedQuantity, setSelectedQuantity ]= useState(0);
+  const { cartProducts, addToCart, removeFromCart } = useContext(CartContext);
+  const flower = useLoaderData();
+  const handleClick = () => {
+    if (selectedQuantity > 0) {
+      addToCart({
+        ...flower,
+        selectedQuantity,
+        totalPriceForItem: `£${(selectedQuantity * parseFloat(flower.price.substring(1))).toFixed(2)}`
+      });
+    } else {
+      const itemToRemove = cartProducts.find((item) => item.id === flower.id);
+      if (itemToRemove) {
+        removeFromCart(flower);
+      }
+    }
+  };
+
+  const quantity = Array.from(Array(20).keys());
+
+  return (
+    <div className='flower-page-container'>
+      <div className='flower-page-image'>
+        <img src={flower.image} width={350} height={350}/>
+      </div>
+      <div className='flower-page-info'>
+        <h2>{flower.name}</h2>
+        <p>{flower.description}</p>
+        <p>{flower.price}</p>
+        <label>Quantity: </label>
+        <select onChange={(e) => setSelectedQuantity(e.target.value)}>
+          <option>0</option>
+          {quantity.map((q)=> (
+            <option key={q} value={q}>{q}</option>
+          ))}
+        </select>
+        <div>
+          <button
+            className='add-to-cart'
+            onClick={handleClick}>Add to cart</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FlowerDetails;
+
+export const flowerDetailsLoader = async ({ params }) => {
+  const { id } = params;
+  const response = await fetch('http://localhost:8000/flowers/' + id);
+  return response.json();
+};
