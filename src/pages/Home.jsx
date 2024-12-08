b63161705e65c4ebc407d54670105b7838860714
Alapan Mukherjee <alapan.mukherjee35@gmail.com>
2024-12-08 03:14:01 +0000

    Refs #4 Style home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,19 @@
-import { Link } from 'react-router-dom';
-import './styles/Home.css'
+import { NavLink } from 'react-router-dom';
+import './styles/Home.css';
+import { useContext } from 'react';
+import { CartContext } from '../layouts/RootLayout';
 
 const Home = () => {
+  const { setNavLinkCls } = useContext(CartContext);
   return (
     <div className='homepage'>
       <div className='homepage-image'>
         <img src='/flowers_home.webp' width={400} height={400}/>
       </div>
-      <div className='homepage-text'>
-        <Link to='/shop' className=''>View our collection</Link>
+      <div className='homepage-text-container'>
+        <div className='homepage-button'>
+          <NavLink to='/shop' className='' onClick={() => setNavLinkCls('/shop')}>View our collection</NavLink>
+        </div>
       </div>
     </div>
   );
